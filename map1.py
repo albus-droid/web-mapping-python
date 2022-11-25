c82ba50cf1a1e143b12fa1eff674b55b7211588a
albus <70963724+albus-droid@users.noreply.github.com>
2022-11-25 16:36:48 +0530

    Added locations of volcanoes in the US

--- map1.py
+++ map1.py
@@ -1,19 +1,23 @@
 import folium
+import pandas
 
 map = folium.Map(location=[38.58, -99.09],
                  zoom_start=6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 
-coordinate_X_Y = [[25.3170, 51.5124], [-25.578599066224918, 27.16070830821991],
-                  [-26.234807275577644, 27.982478141784668], [38.2, -99.1]]
-for coordinates in coordinate_X_Y:
+volcanoData = pandas.read_csv("./Resources/Volcanoes.txt")
+lat = list(volcanoData["LAT"])
+lon = list(volcanoData["LON"])
+
+for lt, ln in zip(lat, lon):
     fg.add_child(
         folium.Marker(
-            location=coordinates,
+            location=[lt, ln],
             popup="Hi I'm a Marker",
             icon=folium.Icon(color="green"),
         )
     )
+
 map.add_child(fg)
 map.save("Map1.html")
