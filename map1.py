fabd543198b34925c62d5b93690fc2d2d788790f
albus <70963724+albus-droid@users.noreply.github.com>
2022-11-26 13:36:03 +0530

    Added elevations as pop-up on the markers

--- map1.py
+++ map1.py
@@ -1,20 +1,21 @@
 import folium
 import pandas
 
-map = folium.Map(location=[38.58, -99.09],
-                 zoom_start=6, tiles="Stamen Terrain")
+map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 
 volcanoData = pandas.read_csv("./Resources/Volcanoes.txt")
+
 lat = list(volcanoData["LAT"])
 lon = list(volcanoData["LON"])
+elev = list(volcanoData["ELEV"])
 
-for lt, ln in zip(lat, lon):
+for lt, ln, el in zip(lat, lon, elev):
     fg.add_child(
         folium.Marker(
             location=[lt, ln],
-            popup="Hi I'm a Marker",
+            popup=folium.Popup(str(el) + "m", parse_html=True),
             icon=folium.Icon(color="green"),
         )
     )
