15af3029a6a3ee8dc351921b2b60f68db1081192
albus <70963724+albus-droid@users.noreply.github.com>
2022-11-26 13:55:28 +0530

    Added extra info on the Map
    Added Volcano names along with a dynamic link that redirects to a Google search result about that volcano

--- map1.py
+++ map1.py
@@ -7,15 +7,23 @@ fg = folium.FeatureGroup(name="My Map")
 
 volcanoData = pandas.read_csv("./Resources/Volcanoes.txt")
 
+html = """
+Volcano name:<br>
+<a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a><br>
+Height: %s m
+"""
+
 lat = list(volcanoData["LAT"])
 lon = list(volcanoData["LON"])
 elev = list(volcanoData["ELEV"])
+name = list(volcanoData["NAME"])
 
-for lt, ln, el in zip(lat, lon, elev):
+for lt, ln, el, name in zip(lat, lon, elev, name):
+    iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100)
     fg.add_child(
         folium.Marker(
             location=[lt, ln],
-            popup=folium.Popup(str(el) + "m", parse_html=True),
+            popup=folium.Popup(iframe),
             icon=folium.Icon(color="green"),
         )
     )
